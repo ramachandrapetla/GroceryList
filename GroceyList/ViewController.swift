c46c0004b459e0acfb0d0ec865173a9f5b48dfb2
ramachandrapetla <ramachandra1996@gmail.com>
2022-09-23 16:18:00 -0500

    Initial Commit

---
+++ GroceyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroceyList
+//
+//  Created by Ramachandra petla on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
