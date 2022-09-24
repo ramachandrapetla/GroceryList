a03c606ecc9997ed5633c68db9463e5bb89a9b08
ramachandrapetla <ramachandra1996@gmail.com>
2022-09-23 19:24:38 -0500

    HomeVC Changes

---
+++ GroceyList/ViewControllers/HomeScreenVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeScreenVC.swift
+//  GroceyList
+//
+//  Created by Ramachandra petla on 9/23/22.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
